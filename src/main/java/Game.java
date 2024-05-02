2837fff4984d51e36d29add43f1da488953cb8e8
AlxandrV <a.vigneaud@gmail.com>
2024-05-02 23:03:18 +0200

    add choice action method

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,22 +1,54 @@
 package main.java;
 
+import java.util.Scanner;
+
 public class Game {
 
+  Board board;
+
   public static void main(String[] args) {
-    Board board = new Board(5, 7);
-    board.setMatrice(new String[5][7]);
+
     Game game = new Game();
-    game.launch(board);
+    game.launch();
   }
 
-  public Board launch(Board board) {
-    // Place le joueur au centre de la map
-    String[][] matrice = board.getMatrice();
-    int row = matrice.length / 2;
-    int column = matrice[0].length / 2;
-    matrice[row][column] = "*";
-    board.setMatrice(matrice);
+  public void launch() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Nombre de ligne : ");
+    int row = Integer.parseInt(scanner.nextLine());
 
-    return board;
+    System.out.print("Nombre de colonne : ");
+    int column = Integer.parseInt(scanner.nextLine());
+
+    this.board = new Board(row, column);
+    this.choiceAction();
+
+    scanner.close();
   }
+
+  public void choiceAction() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("\n-----\nActions disponible :\n-----\nTour suivant [1]\nQuitter[0]\n");
+    int action = Integer.parseInt(scanner.nextLine());
+
+    String choice = this.action(action);
+    System.out.print("\n" + choice);
+    if ("Tour suivant".equals(choice)) {
+      this.choiceAction();
+    }
+  }
+
+  public String action(int inputValue) {
+    switch (inputValue) {
+      case 1:
+        if (this.board != null) {
+          System.out.print(this.board.toString());
+        }
+        return "Tour suivant";
+      case 0:
+        return "Fin de partie";
+    }
+    return "";
+  }
+
 }
--- src/test/GameTest.java
+++ src/test/GameTest.java
@@ -1,24 +1,27 @@
 package test;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-import main.java.Board;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import main.java.Game;
 
 class GameTest {
 
-  @Test
-  void launchTest() {
+  @ParameterizedTest
+  @MethodSource
+  void actionsTest(int inputValue, String expectedValue) {
+    // Given
     Game game = new Game();
-    Board board = new Board(5, 7);
+    // When
+    String action = game.action(inputValue);
+    // Then
+    Assertions.assertEquals(expectedValue, action);
+  }
 
-    Board boardExpected = game.launch(board);
-    String[][] matriceExpected = boardExpected.getMatrice();
-
-    int row = matriceExpected.length / 2;
-    int column = matriceExpected[0].length / 2;
-
-    Assertions.assertEquals("*", matriceExpected[row][column]);
+  private static Stream<Arguments> actionsTest() {
+    return Stream.of(Arguments.of(1, "Tour suivant"), Arguments.of(0, "Fin de partie"),
+        Arguments.of(6, ""));
   }
 }
