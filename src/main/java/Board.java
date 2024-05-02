ccea2a978c3d4d84a054278b58701d1df8b6ffee
Bastien Chanez <bastien.chanez@gmail.com>
2024-05-02 17:47:49 +0200

    empty board

---
+++ src/main/java/Board.java
@@ -0,0 +1,42 @@
+package main.java;
+
+public class Board {
+
+  private String[][] matrice;
+  private int nbRow;
+  private int nbColumn;
+
+  public Board(int nbRow, int nbColumn) {
+    this.nbRow = nbRow;
+    this.nbColumn = nbColumn;
+
+    initMatrice();
+  }
+
+  private void initMatrice() {
+    matrice = new String[nbRow][nbColumn];
+    for (int i = 0; i < nbRow; i++) {
+      for (int j = 0; j < nbColumn; j++) {
+        matrice[i][j] = "*";
+      }
+    }
+  }
+
+
+
+  @Override
+  public String toString() {
+    String display = "";
+
+    for (int i = 0; i < nbRow; i++) {
+      for (int j = 0; j < nbColumn; j++) {
+        display += matrice[i][j];
+      }
+      if (i < nbRow - 1) {
+        display += "\n";
+      }
+    }
+
+    return display;
+  }
+}
