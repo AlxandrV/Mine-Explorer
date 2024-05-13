c945dfce007cbda4eb6da356662cb0ad4ce76e03
AlxandrV <a.vigneaud@gmail.com>
2024-05-13 21:19:49 +0200

    add choice direction in menu

--- src/main/java/com/project/Game.java
+++ src/main/java/com/project/Game.java
@@ -22,7 +22,7 @@ public class Game {
       if (action == 0) {
         gameRunning = false;
       } else if (action == 1) {
-        moveToDirection();
+        String direction = menu.moveToDirection();
       }
     } while (gameRunning);
   }
@@ -31,27 +31,4 @@ public class Game {
     return gameRunning;
   }
 
-  private void moveToDirection() {
-    System.out.println("Se déplacer vers :\n" + "Haut : z | Bas : s | Gauche : q | Droite : d");
-    String direction = scanner.next();
-
-    int[] position = board.playerPosition();
-    Room[][] matrice = board.getMatrice();
-    Player player = board.getPlayer();
-
-    switch (direction) {
-      case "z":
-        player.setRoom(matrice[position[0] - 1][position[1]]);
-        break;
-      case "s":
-        player.setRoom(matrice[position[0] + 1][position[1]]);
-        break;
-      case "q":
-        player.setRoom(matrice[position[0]][position[1] - 1]);
-        break;
-      case "d":
-        player.setRoom(matrice[position[0]][position[1] + 1]);
-        break;
-    }
-  }
 }
